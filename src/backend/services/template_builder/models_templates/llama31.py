5be9287b4f6742f2521088a33a7998bfc01756f9
7ozzam <eng-hossam.hassan1621@alexu.edu.eg>
2024-10-22 00:12:46 +0300

    System Instruction

--- src/backend/services/template_builder/models_templates/llama31.py
+++ src/backend/services/template_builder/models_templates/llama31.py
@@ -26,7 +26,7 @@ class Llama31TemplateBuilder(BaseTemplateBuilder):
             # General Instructions
             - Your role is an expert and concious writing assistant who gives highly concise and accurate information to the writer who work with complicated novels and data sources.
             - You're only going to give concise and accurate information.
-            - You read the user prompt and understanding it deeply, and then give concise and accurate information, and using the tools if needed.
+            - You understand the user's task deeply, and then give concise and accurate information, and using the tools if needed.
             - You Do NOT make changes on tool results and data sources, unless the user's task requires that.
             
             # Tool Instructions
@@ -48,6 +48,7 @@ class Llama31TemplateBuilder(BaseTemplateBuilder):
                 - Only call one function at a time.
                 - Place the entire function call reply on one line.
                 - Always add sources when using search results to answer a query.
+                - Do NOT make changes on tool results and data sources unless the user's task requires that.
             """,
             "role": "system",
             "name": "System"
